1521b66dbae2e82b4a91f2ed6ec2d528b84a8c54
embylee <embylee@gmail.com>
2026-07-17 11:53:13 -0400

    适配 ImmortalWrt 25.12 并修复多个工作流与脚本 bug
    25.12 适配：
    - 23.05 工作流迁移为跟踪 openwrt-25.12 分支，单独维护 .diffconfig2512，
      不影响 24.10 现有配置
    - 配置改为最小种子集（内核选项等交由 make defconfig 自动生成），
      并按官方 25.12.1 软件源逐包核实可用性
    - rename.sh 内核版本判断由写死的 5.4/5.15 改为动态读取，同时修复了
      24.10 因内核为 6.6 导致固件重命名一直失效的问题
    
    软件包调整：
    - 移除 homeproxy（保留 passwall/openclash/nikki）、statistics 及
      collectd 依赖链（保留 netdata）、docker 全套、ruby 全家桶、
      声卡驱动，均以 "is not set" 标注而非删行，方便日后恢复
    - 新增 kmod-tcp-bbr、luci-app-watchcat、irqbalance
    - 删除失效的 mihomo feed（已被 nikki 取代）及重复的 svn 拉包逻辑
    - 移除已失效的 romupdate 及其残留的空壳更新脚本、死别名
    
    CI 修复：
    - Telegram 通知补上 if: always()，构建失败也能收到通知；
      secret 统一为 TELEGRAM_BOT_TOKEN；修正树莓派失败文案
    - 合并两个互相矛盾的“删除旧 runs”步骤为一个，改用内置
      GITHUB_TOKEN，避免因缺失 PAT_TOKEN 导致最后一步误报失败
    - preset-clash-core-x86.sh 增加 set -e，下载失败时不再将空的
      clash 内核打包进固件
    - 第三方 Action 固定到具体版本/commit，不再跟随 @main/@master 浮动
    
    清理：
    - 删除无人引用的 config/.cudyconfig、config/.diffconfig0828
    - 删除死变量 UPLOAD_COWTRANSFER、UPLOAD_WETRANSFER

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,6 @@
 sed -i "/helloworld/d" "feeds.conf.default"
 #sed -i "/luci/d" "feeds.conf.default"
 echo "src-git helloworld https://github.com/fw876/helloworld.git" >> "feeds.conf.default"
-echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
 echo "src-git adguard https://github.com/Jacky-Bruse/openwrt-adguardhome.git" >> "feeds.conf.default"
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 #echo "src-git nekoclash https://github.com/Thaolga/luci-app-nekoclash.git" >> "feeds.conf.default"
@@ -24,8 +23,6 @@ echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >>
 #sed -i '$a src-git adguard https://github.com/281677160/openwrt-package.git;adguard' feeds.conf.default
 #svn co https://github.com/281677160/openwrt-package/trunk/luci-app-clash package/luci-app-clash
 #svn co https://github.com/281677160/openwrt-package/branches/19.07/luci-app-eqos package/luci-app-eqos
-svn co https://github.com/kiddin9/openwrt-packages/branches/master/adguardhome adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/branches/master/luci-app-adguardhome luci-app-adguardhome
 
 
 # Add a feed source
@@ -35,16 +32,12 @@ mkdir -p files/etc/
 touch files/etc/Lee_version
 mkdir wget
 touch wget/DISTRIB_REVISION1
-touch wget/DISTRIB_REVISION3
-touch files/usr/share/Check_Update.sh
-touch files/usr/share/Lee.sh
 
 # backup config
 cat>> package/base-files/files/lib/upgrade/keep.d/base-files-essential<<-EOF
 /etc/config/dhcp
 /etc/config/xray
 /etc/config/sing-box
-/etc/config/romupdate
 /etc/config/passwall_show
 /etc/config/passwall_server
 /etc/config/passwall
@@ -76,56 +69,28 @@ rm -rf bin/targets/x86/64/immortalwrt-x86-64-generic-ext4-combined-efi.img.gz
 rm -rf bin/targets/x86/64/immortalwrt-x86-64-generic-ext4-combined.img.gz
 sleep 2
 rename_version=`cat files/etc/Lee_version`
-str1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如5.10
-ver54=`grep "LINUX_VERSION-5.4 ="  include/kernel-5.4 | cut -d . -f 3`
-ver515=`grep "LINUX_VERSION-5.15 ="  include/kernel-5.15 | cut -d . -f 3`
+str1=`grep "KERNEL_PATCHVER:="  target/linux/x86/Makefile | cut -d = -f 2` #判断当前默认内核版本号如6.12
+kpatch=`grep "LINUX_VERSION-${str1} ="  include/kernel-${str1} | cut -d . -f 3` #取小版本号，通用适配任意内核系列
 sleep 2
-if [ "$str1" = "5.4" ];then
-  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver54}_sta_Lee.img.gz
-  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver54}_uefi-gpt_sta_Lee.img.gz
-elif [ "$str1" = "5.15" ];then
-  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver515}_sta_Lee.img.gz
-  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${ver515}_uefi-gpt_sta_Lee.img.gz
+if [ -e bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz ];then
+  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined.img.gz       bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${kpatch}_sta_Lee.img.gz
+  mv  bin/targets/x86/64/immortalwrt-x86-64-generic-squashfs-combined-efi.img.gz   bin/targets/x86/64/immortalwrt_x86-64-${rename_version}_${str1}.${kpatch}_uefi-gpt_sta_Lee.img.gz
 fi
-ls bin/targets/x86/64 | grep "gpt_sta_Lee.img" | cut -d - -f 3 | cut -d _ -f 1-2 > wget/op_version1
-#md5
-ls -l  "bin/targets/x86/64" | awk -F " " '{print $9}' > wget/open_sta_md5
-sta_version=`grep "_uefi-gpt_sta_Lee.img.gz" wget/open_sta_md5 | cut -d - -f 3 | cut -d _ -f 1-2`
-immortalwrt_sta=immortalwrt_x86-64-${sta_version}_sta_Lee.img.gz
-immortalwrt_sta_uefi=immortalwrt_x86-64-${sta_version}_uefi-gpt_sta_Lee.img.gz
-cd bin/targets/x86/64
 exit 0
 EOF
 
-cat>files/usr/share/Check_Update.sh<<-\EOF
-#!/bin/bash
-# 此处是原来检查固件更新并升级的部分，已删除
-exit 0
-EOF
 
 cat>Lee.sh<<-\EOOF
 #!/bin/bash
-Lee_version="`date '+%y%m%d%H%M'`_sta_Lee" 
-echo $Lee_version >  wget/DISTRIB_REVISION1 
-echo $Lee_version | cut -d _ -f 1 >  files/etc/Lee_version  
+Lee_version="`date '+%y%m%d%H%M'`_sta_Lee"
+echo $Lee_version >  wget/DISTRIB_REVISION1
+echo $Lee_version | cut -d _ -f 1 >  files/etc/Lee_version
 new_DISTRIB_REVISION=`cat  wget/DISTRIB_REVISION1`
-#
-grep "Check_Update.sh"  package/emortal/default-settings/files/99-default-settings
-if [ $? != 0 ]; then
-	sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
-	cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
-	sed -i '$ a alias lee="sh /usr/share/Check_Update.sh"' /etc/profile
+# 将固件版本号写入系统描述（网页概况页显示）
+sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
+cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
 	sed -i '/DISTRIB_DESCRIPTION/d' /etc/openwrt_release
 	echo "DISTRIB_DESCRIPTION='$new_DISTRIB_REVISION'" >> /etc/openwrt_release
 	exit 0
 	EOF
-fi
-grep "Lee.sh"  package/emortal/default-settings/files/99-default-settings
-if [ $? != 0 ]; then
-	sed -i 's/exit 0/ /'  package/emortal/default-settings/files/99-default-settings
-	cat>> package/emortal/default-settings/files/99-default-settings<<-EOF
-	sed -i '$ a alias lenyu-auto="sh /usr/share/Lee.sh"' /etc/profile
-	exit 0
-	EOF
-fi
 EOOF
